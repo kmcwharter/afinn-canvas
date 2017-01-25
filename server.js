a9d16ea1602634b0c4831da479392fd4fdaf5de7
kmcwharter <Sincerily 2008@KristinsMacBook.local>
2017-01-24 21:46:08 -0800

    shiffman

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen('http://kmcwharter.github.io');
+var server = app.listen(process.env.PORT || 3000, listen);
+
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('example app listening at http://' + host + ':' + port);
+}
 
 app.use(express.static('public'));
 
