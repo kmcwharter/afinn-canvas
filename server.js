61b1589fcf9bd63b0d6b8bc623883448f32f226c
kmcwharter <Sincerily 2008@KristinsMacBook.local>
2017-01-24 21:42:28 -0800

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen('http://kmcwharter.github.io');
 
 app.use(express.static('public'));
 
